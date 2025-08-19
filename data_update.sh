78ede47620c2541544ceb2dbd9ddac58d391064f
Weidian Zhan <a3800599@163.com>
2025-08-19 20:14:14 +0800

    initial

---
+++ data_update.sh
@@ -0,0 +1,11 @@
+cd ~
+SAVE_PATH=qlib_bin.tar.gz
+DATA_PATH=qlib_data/cn_data
+
+wget https://github.com/chenditc/investment_data/releases/latest/download/qlib_bin.tar.gz
+
+rm -rf ${DATA_PATH}
+mkdir -p ${DATA_PATH}
+
+tar -zxvf qlib_bin.tar.gz -C ${DATA_PATH} --strip-components=1
+rm -f qlib_bin.tar.gz
